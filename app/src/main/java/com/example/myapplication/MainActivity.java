a4634c57517141ce549dbd88699d8a3e6888cb2c
samy-Elfakhrany <user@Samy-Elfakhrany>
2022-08-13 23:42:13 +0200

    nav_bar is ready (with sum bugs)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+        replaceFragment(new exercise_Fragment());
+        binding.bottomNavView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId())
+            {
+                case R.id.exercise:
+                    replaceFragment(new exercise_Fragment());
+                    break;
+                case R.id.foods:
+                    replaceFragment(new foodFragment());
+
+
+                    break;
+                case R.id.bmi:
+                    replaceFragment(new bmiFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
