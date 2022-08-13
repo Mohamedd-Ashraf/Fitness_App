50d797c87f282d4469a85132d6b530dc1af287b6
Youssef Shehata <31982488+Youssef-Shehata@users.noreply.github.com>
2022-08-13 14:29:34 +0200

    Update Food.java

--- app/src/main/java/com/example/myapplication/Food.java
+++ app/src/main/java/com/example/myapplication/Food.java
@@ -1,5 +1,25 @@
 package com.example.myapplication;
 
 public class Food {
+    String name;
+    String calories;
+    String protein_g;
+    String carbohydrates_total_g;
+
+
+
+
+ public String getName() {
+        return name;
+    }
+     public String getCalories() {
+        return calories;
+    }
+     public String getProtien() {
+        return protein_g;
+    }
+     public String getCarb() {
+        return carbohydrates_total_g;
+    }
 
 }
