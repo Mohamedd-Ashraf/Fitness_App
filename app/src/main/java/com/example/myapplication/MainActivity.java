e871d2a971193c7c51524b0806a29436661db3ca
samy-Elfakhrany <user@Samy-Elfakhrany>
2022-08-15 17:51:38 +0200

    navigtion bar completed

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
@@ -13,27 +16,34 @@ import com.example.myapplication.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
         replaceFragment(new exercise_Fragment());
         binding.bottomNavView.setOnItemSelectedListener(item -> {
             switch (item.getItemId())
             {
-                case R.id.exercise:
+                case R.id.Exercises_item:
                     replaceFragment(new exercise_Fragment());
-                    break;
-                case R.id.foods:
+                    return true;
+                case R.id.Foods_item:
                     replaceFragment(new foodFragment());
 
 
-                    break;
-                case R.id.bmi:
+                    return true;
+                case R.id.Bmi_item:
                     replaceFragment(new bmiFragment());
-                    break;
+                    return true;
             }
+
             return true;
         });
     }
